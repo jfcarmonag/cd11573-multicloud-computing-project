7a003c335c36771c4fdc0988c0aa1bda8a771860
Juan_Felipe <jfcarmonag@gmail.com>
2023-03-21 10:38:19 -0500

    add login pswd

--- starter/azure/main.tf
+++ starter/azure/main.tf
@@ -36,7 +36,8 @@ resource "azurerm_mssql_server" "udacity" {
   resource_group_name          = data.azurerm_resource_group.udacity.name
   location                     = data.azurerm_resource_group.udacity.location
   version                      = "12.0"
-  
+  administrator_login          = "4dm1n157r470r"
+  administrator_login_password = "4-v3ry-53cr37-p455w0rd"
   
 }
 
