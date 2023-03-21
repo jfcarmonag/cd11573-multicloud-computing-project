8a915dc2aa6bb6994ccc0b27ad57ddc9101aec9a
Juan_Felipe <jfcarmonag@gmail.com>
2023-03-21 10:20:05 -0500

    fix function app

--- starter/azure/main.tf
+++ starter/azure/main.tf
@@ -38,7 +38,31 @@ resource "azurerm_mssql_server" "udacity" {
   
 }
 
-resource "azurerm_function_app" "udacity" {
+
+resource "azurerm_storage_account" "example" {
+  name                     = "juancudacitystorage"
+  resource_group_name      = data.azurerm_resource_group.example.name
+  location                 = data.azurerm_resource_group.example.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+resource "azurerm_app_service_plan" "example" {
+  name                = "azure-functions-test-service-plan"
+  location            = data.azurerm_resource_group.example.location
+  resource_group_name = data.azurerm_resource_group.example.name
+
+  sku {
+    tier = "Standard"
+    size = "S1"
+  }
+}
+
+resource "azurerm_function_app" "example" {
   name                       = "udacity-juanc-azure-dotnet-apps"
-  resource_group_name        = data.azurerm_resource_group.udacity.name
+  location                   = data.azurerm_resource_group.example.location
+  resource_group_name        = data.azurerm_resource_group.example.name
+  app_service_plan_id        = azurerm_app_service_plan.example.id
+  storage_account_name       = azurerm_storage_account.example.name
+  storage_account_access_key = azurerm_storage_account.example.primary_access_key
 }
